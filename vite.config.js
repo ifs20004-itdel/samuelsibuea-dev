f367d4c3427abd6430a7d94713680db8f2b0e7d0
ifs20004-itdel <samuelsibuea82@gmail.com>
2024-02-04 17:39:56 +0700

    fix: bundle

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
           ],
   build:{
     rollupOptions:{
-      external: ['react-router-dom', 'react-dom']
+      external: ['react-router-dom']
     }
   },
   publicDir: 'public',
