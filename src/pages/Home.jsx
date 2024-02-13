600ab35ba0c4ae536f1ca2a99a66cd6a743b6aab
ifs20004-itdel <samuelsibuea82@gmail.com>
2024-02-13 10:46:32 +0700

    refactor: seperate dotMesh in Home.jsx into single class Planet.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,19 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
-import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import starsTexture from '/images/stars.jpg';
+import Planet from '../components/three.js/Planet.jsx';
 
-const SCENE_BACKGROUND_COLOR = 0x000000;
-
-const CAMERA_FOV = 12;
+const CAMERA_FOV = 45;
 const CAMERA_NEAR = 100;
 const CAMERA_FAR = 1000;
-const CAMERA_X = 0;
+const CAMERA_X = -90;
 const CAMERA_Y = 0 ;
 const CAMERA_Z = innerHeight/2;
 
-const SPHERE_RADIUS = 20;
-// const LATITUDE_COUNT = 30;
-// const LONGITUDE_COUNT = 50;
-// const DOT_DENSITY = 0.5;
-
-const DOT_SIZE = 0.2;
-const DOT_COLOR = 0xc6cdd5   ;
-
 const Home = () => {
   useEffect(() => {
-    const renderer = new THREE.WebGLRenderer(
-    );
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
@@ -42,88 +32,78 @@ const Home = () => {
 
     const scene = new THREE.Scene();
     
-    scene.background = new THREE.Color(SCENE_BACKGROUND_COLOR);
-    
-    const dotGeometries = [];
-    const vector = new THREE.Vector3();
+    const cubeTextureLoader = new THREE.CubeTextureLoader();
 
-    for(var i = 0; i<1600; i++){
-        const dotGeometry = new THREE.CircleGeometry(DOT_SIZE, 5);
-        const phi = Math.random()*Math.PI;
-        const theta = Math.random()*2*Math.PI;
+    scene.background = cubeTextureLoader.load([
+        starsTexture,
+        starsTexture,
+        starsTexture,
+        starsTexture,
+        starsTexture,
+        starsTexture
+    ]);
 
-        vector.setFromSphericalCoords(SPHERE_RADIUS, phi, theta);
-        dotGeometry.lookAt(vector);
-        dotGeometry.translate(vector.x, vector.y, vector.z);
-        dotGeometries.push(dotGeometry);
-    }
+    // Construct planet 1
+    const planet1 = new Planet(75).createSphere(1600);
+    planet1.position.set(200,20,0);
+    scene.add(planet1);
 
-    // other pattern
-    
-    // for(let lat=0; lat<=LATITUDE_COUNT; lat+=1){
-    //     for(let lng = 0; lng<LONGITUDE_COUNT; lng+=1){
-    //         const dotGeometry = new THREE.CircleGeometry(DOT_SIZE, 5);
-    //         const phi = (Math.PI/LATITUDE_COUNT)*lat;
-    //         const theta = ((2*Math.PI)/LONGITUDE_COUNT)*lng;
+    // Construct moon 1
+    const moon1 = new Planet(5).createSphere(200);
+    moon1.position.set(100,0,10);
+    const moon = new THREE.Object3D();
+    moon.add(moon1);
+    moon.position.set(200,20,0);
+    scene.add(moon);
 
-    //         vector.setFromSphericalCoords(SPHERE_RADIUS, phi, theta);
-    //         dotGeometry.lookAt(vector);
-    //         dotGeometry.translate(vector.x, vector.y, vector.z);
-    //         dotGeometries.push(dotGeometry);
-    //     }
-    // }
-
-    const mergeDotGeometries = BufferGeometryUtils.mergeBufferGeometries(dotGeometries);
-    const dotMaterial =  new THREE.MeshBasicMaterial({
-        color: DOT_COLOR,
-        side: THREE.DoubleSide
-    });
-
-    const dotMesh = new THREE.Mesh(mergeDotGeometries, dotMaterial);
-    
-    scene.add(dotMesh);
+    // Construct planet 2
+    const planet2 = new Planet(100).createSphere(1300); 
+    planet2.position.set(-150,190,-550);
+    scene.add(planet2);
 
     const animate = (time)=>{
-        time *= 0.001;
-        
-        dotMesh.rotation.y = time*0.1;
+        time *= 0.005;
+        planet1.rotation.y = time*0.1;
+        planet2.rotation.y = time*0.2;
+
+        moon1.rotation.y = time*0.5
+        moon.rotation.y = time*0.3;
+
         controls.update();
         renderer.render(scene, camera);
         
         requestAnimationFrame(animate);
     }
 
-    const greetings = document.createElement('div');
-    greetings.className = 'absolute top-1/2 left-24 text-white font-poppins bg-transparent';
 
+    // Greetings
+    const greetings = document.createElement('div');
+    greetings.className = 'absolute top-1/4 md:top-1/2 left-5 md:left-24 text-white font-poppins bg-transparent';
     
     const name = document.createElement('h1');
     name.innerHTML = 'Samuel Immanuel Herlinton Sibuea';
-    name.className = 'pb-5 font-bold text-3xl bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient drop-shadow-x'
+    name.className = 'pb-5 font-bold text-xl md:text-3xl bg-clip-text text-transparent bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient drop-shadow-x'
 
     const greet = document.createElement('p');
     greet.innerHTML = 'Hi, Fellow Stalker! ✋';
-    greet.className = 'pb-1 font-bold text-2xl '
+    greet.className = 'pb-1 font-bold text-xl md:text-2xl '
 
     const occupation = document.createElement('p');
     occupation.innerHTML = 'I\'m a Software Engineer';
 
-    occupation.className = 'font-bold text-2xl'
+    occupation.className = 'font-bold text-xl md:text-3xl'
 
     greetings.appendChild(name);
     greetings.appendChild(greet);
     greetings.appendChild(occupation);    
 
-
     const copyright = document.createElement('p');
     copyright.innerHTML = '©Copyright 2024'
-    copyright.className = 'absolute bottom-3 left-24 text-white'
+    copyright.className = 'absolute bottom-3 left-3 md:left-24 text-white'
     
-
     document.body.appendChild(greetings);
     document.body.appendChild(copyright);
     
-
     requestAnimationFrame(animate);
 
     return () => {
