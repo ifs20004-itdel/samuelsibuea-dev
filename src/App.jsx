40c53b3e0069d79bc105ce9d91b68b6cd94e63d2
ifs20004-itdel <samuelsibuea82@gmail.com>
2024-11-04 20:08:26 +0700

    feat: add preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
-import {About, Achievement, Experience, Home, Projects} from './pages';
+import React, { useEffect, useState } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { About, Achievement, Experience, Home, Projects } from "./pages";
+import Loader from "./components/Loader/Loader";
 
-const App = () => {
-    return (
-        <main className='bg-slate-300/20'>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/about' element={<About/>}/>
-                    <Route path='/achievement' element={<Achievement/>}/> 
-                    <Route path='/experience' element={<Experience/>}/>
-                    <Route path='/projects' element={<Projects/>}/>
-                </Routes>
-            </Router>
-        </main>
-    );
-}
+const AppContent = () => {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <main className="bg-slate-300/20">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/achievement" element={<Achievement />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </main>
+  );
+};
+
+const App = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
 
 export default App;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import plant from "/images/plant.gif";
+
+function Loader() {
+  return (
+    <div className=" bg-sky-600">
+      <div className="h-screen flex flex-col items-center justify-center">
+        <img src={plant} alt="preload" width={60} />
+        <p className="font-wicked text-white tracking-wider py-2">Loading ...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
